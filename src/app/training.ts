835a1296f8ca96c0c5f34fbedd54fe2d405b0e28
chiksa14 <muhtabalievdamir@gmail.com>
2026-04-08 08:18:16 +0300

    change code

--- src/app/training.ts
+++ src/app/training.ts
@@ -8,15 +8,50 @@ interface IUser {
 }
 
 const users: IUser[] = [
-  { id: 1, name: 'Alice', age: 25, city: 'alice@example.com', jobs: 'developer', experience: 10},
-  { id: 2, name: 'Bob', age: 30, city: 'bob@example.com', jobs: 'doctor', experience: 5 },
-  { id: 3, name: 'Charlie', age: 35, city: 'charlie@example.com', jobs: 'military', experience: 1 },
-  { id: 4, name: 'Diana', age: 28, city: 'diana@example.com', jobs: 'teacher', experience: 3 },
-  { id: 5, name: 'Eve', age: 22, city: 'eve@example.com', jobs: 'manager', experience: 2 }
+  {
+    id: 1,
+    name: 'Alice',
+    age: 25,
+    city: 'Москва',
+    jobs: 'developer',
+    experience: 10
+  },
+  {
+    id: 2,
+    name: 'Bob',
+    age: 30,
+    city: 'Астрахань',
+    jobs: 'doctor',
+    experience: 5
+  },
+  {
+    id: 3,
+    name: 'Charlie',
+    age: 35,
+    city: 'Волгоград',
+    jobs: 'military',
+    experience: 1
+  },
+  {
+    id: 4,
+    name: 'Diana',
+    age: 28,
+    city: 'Краснодар',
+    jobs: 'teacher',
+    experience: 3
+  },
+  {
+    id: 5,
+    name: 'Eve',
+    age: 22,
+    city: 'Воронеж',
+    jobs: 'manager',
+    experience: 2
+  }
 ];
 
-const usersOver25 = users.filter(user => user.age > 25);
-console.log('Пользователи старше 25:', usersOver25);
+const eligibleUsers: IUser[] = users.filter(user => user.age > 25);
+console.log('Пользователи старше 25:', eligibleUsers );
 
 interface IKid extends IUser {
   schoolClass: number;
@@ -27,8 +62,8 @@ let uploadStatus: "loading" | "success" | "error";
 
 type TextFormat = 'uppercase' | 'lowercase' | 'capitalize';
 
-function calc(a: number, b:number): number{
-  return a+b
+function calc(a: number, b: number): number{
+  return a + b;
 }
 
 calc(1, 2)
